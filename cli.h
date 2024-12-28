e89e625f2c432d145ea790baac3cd1ef09ac7c3a
Federico D. Ceccarelli <fededc88@gmail.com>
2024-12-28 00:25:42 -0500

    set up parser arguments configurations to save memory

--- cli.h
+++ cli.h
@@ -3,9 +3,21 @@
 
 #include <CommandParser.h>
 
-#define LINE_LENGTH 80 /* default line length */
+#define LINE_LENGTH 40 /* default line length */
 
-typedef CommandParser<> parser;
+/**
+ * All of the template arguments below are optional, but it is useful to adjust
+ * them to save memory (by lowering the limits) or allow larger inputs (by
+ * increasing the limits)
+ * limit number of commands to at most 3
+ * limit number of arguments per command to at most 2
+ * limit length of command names to 10 characters
+ * limit size of all arguments to 15 bytes (e.g., the argument "\x41\x42\x43" uses
+ *    14 characters to represent the string but is actually only 3 bytes, 0x41,
+ *    0x42, and 0x43)
+ * limit size of response strings to LINE_LENGTH bytes
+ */
+typedef CommandParser<3, 2, 10, 15, LINE_LENGTH> parser;
 
 class ttcli: public parser
 {
