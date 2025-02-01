1ed6dbaf23faacb14b9e3074a100f74463abd073
Federico D. Ceccarelli <fededc88@gmail.com>
2025-01-31 21:53:00 -0500

    First minimal implementations of AS5040
    for compiling with the local copy of the library do:
    
    arduino-cli compile -b arduino:avr:uno --library <path to lib>/AS5040/

--- TTsaurus.ino
+++ TTsaurus.ino
@@ -10,10 +10,19 @@
 #include "cli.h"
 #include "serial.h"
 
+#include <SPI.h>
+#include "AS5040.h"
+
 //Defines
 #define neutral 0
 #define START_CMD 'g'
 #define STOP_CMD 's' 
+
+/* Pins definitions */
+#define CSpin 10
+#define MOSIpin 11
+#define MISOpin 12
+#define CLKpin 13
 
 //Working Data 
 int controlData = 0;
@@ -24,38 +33,51 @@ Servo myservo;
 /* Command Line interface */
 ttcli cli;
 
+/* Instance AS5040 magnetic position sensor */
+AS5040Class AS5040(CSpin, CLKpin, MISOpin, MOSIpin);
+
 void setup() {
-  // put your setup code here, to run once:
+    // put your setup code here, to run once:
 
-  /* Initialize Serial */
-  serial_init();
+    /* Initialize Serial */
+    serial_init();
 
-  myservo.attach(9);       // attaches the servo on pin 9 to the servo object
+    myservo.attach(9);       // attaches the servo on pin 9 to the servo object
 
-  /* Send Start RARW!! */
-  cli.begin();
+    /* set the CSpin as an output */
+    pinMode(CSpin, OUTPUT);
+
+    SPI.begin();
+    AS5040.begin(&SPI, SPISettings(AS5040_CLKAREAD, MSBFIRST,SPI_MODE1));
+
+    /* Send Start RARW!! */
+    cli.begin();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+    // put your main code here, to run repeatedly:
 
     /* Command Line Interfase runner */
     cli.run();  
 
-/*
-  if (controlData == START_CMD)
-  {
-    myservo.write(180);
-    Serial.print("Servo: ");
-    Serial.println(myservo.read());
-  }
-  else if (controlData == STOP_CMD)
-  {
-    myservo.write(95);
-    Serial.print("Servo: ");
-    Serial.println(myservo.read());
-  }
-*/
+    Serial.println(AS5040.readAbsolutePosition());
+
+    delay(1000);
+
+    /*
+       if (controlData == START_CMD)
+       {
+       myservo.write(180);
+       Serial.print("Servo: ");
+       Serial.println(myservo.read());
+       }
+       else if (controlData == STOP_CMD)
+       {
+       myservo.write(95);
+       Serial.print("Servo: ");
+       Serial.println(myservo.read());
+       }
+     */
 
 }
 
